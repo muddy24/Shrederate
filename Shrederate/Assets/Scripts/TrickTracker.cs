593934ef57604ec46659021d98c71d19f41aa2cb
Sam <sambskn@gmail.com>
2022-03-02 22:27:59 -0600

    add trick data structure and add/remove for tricks -sb

--- Shrederate/Assets/Scripts/TrickTracker.cs
+++ Shrederate/Assets/Scripts/TrickTracker.cs
@@ -1,14 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrickTracker : MonoBehaviour
 {
-    private float trickStartTime;
-    private float trickStartRotation;
     public GameObject player;
+    public GameObject trickPrefab;
+    public GameObject uiCanvas;
 
     public float uprightLandingThreshold = 0.6f;
+    public float trickUILinger = 0.75f; // how long a trcik stay son the ui before dissapearing
+
+    List<GameObject> trickPrefabs;
+    List<TrickData> trickHistory;
+
+    public class TrickData
+    {
+        public float startTime;
+        public Transform startTransform;
+        public bool isComplete;
+        public bool stuckLanding;
+        public float endTime;
+        public Transform endTransform;
+        public int prefabID;
+
+        public TrickData(Transform initTransform, int fabID)
+        {
+            startTime = Time.time;
+            startTransform = initTransform;
+            prefabID = fabID;
+            isComplete = false;
+        }
+
+        public void EndTrick(bool isUpright)
+        {
+            endTime = Time.time;
+            isComplete = true;
+            stuckLanding = isUpright;
+        }
+    }
 
     
     // Start is called before the first frame update
@@ -17,29 +48,83 @@ public class TrickTracker : MonoBehaviour
         GameEvents.current.onAirtimeStart += OnAirtime;
         GameEvents.current.onLand += OnLanding;
 
+        trickPrefabs = new List<GameObject>();
+        trickHistory = new List<TrickData>();
+
     }
 
     private void OnAirtime() {
-        trickStartTime = Time.time;
+        
+        GameObject newTrick = Instantiate(trickPrefab, new Vector3(160,0,0), Quaternion.identity);
+        newTrick.transform.SetParent(uiCanvas.transform, false);
+
+        trickPrefabs.Add(newTrick);
+        trickHistory.Add(new TrickData(player.transform, newTrick.GetInstanceID()));
+
+        // shoudl call update function that bumps all old ones up a little bit
     }
 
     private void OnLanding(float totalJumpTime) {
         // Debug.Log("frog clogs");
-        // check if play stuck the landing --  are they upright?
+        if (trickHistory.Count <= 0) {
+            return;
+        } 
+
+        // check if player stuck the landing --  are they upright?
+        // TODO: make this take the slope of the ground into account
         bool isUpright = player.transform.up.y > uprightLandingThreshold;
         if (isUpright) 
         {
-            Debug.Log("ayo you did it boss");
+            trickHistory[trickHistory.Count - 1].EndTrick(true);
         } else 
         {
-            Debug.Log("bro you DID NOT stick it");
+            trickHistory[trickHistory.Count - 1].EndTrick(false);
+            GameObject finishedTrick = trickPrefabs[trickHistory.Count - 1];
+            Text trickText = finishedTrick.transform.GetChild(0).gameObject.GetComponent<Text>();;
+            trickText.text = "ayo you biffed";
+            Text trickValue = finishedTrick.transform.GetChild(1).gameObject.GetComponent<Text>();;
+            trickValue.text = "";
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         // check if trick is expired then use Destroy(gameObj)  
+        foreach (GameObject fab in trickPrefabs) 
+        {
+            // find matching data for prefab
+            TrickData trick = trickHistory.Find(x => x.prefabID == fab.GetInstanceID());
+
+            if (trick == null) 
+            {
+                Destroy(fab); // don't need any no-good, no-data-having trick in these parts
+            }
+
+            // update values for exisitng tricks
+            if (!trick.isComplete) 
+            {
+                Text trickText = fab.transform.GetChild(0).gameObject.GetComponent<Text>();;
+                Text trickValueText = fab.transform.GetChild(1).gameObject.GetComponent<Text>();;
+                float currentAirtime = Time.time - trick.startTime;
+                trickValueText.text = currentAirtime.ToString("F1") + "s";
+                if (currentAirtime > 2.0f) 
+                {
+                    trickText.text = "Nice Air";
+                    // play sound too
+                } else if (currentAirtime > 5.0f) 
+                {
+                    trickText.text = "Mega Air";
+                }
+            }
+
+            // remove 'old tricks'
+            if (trick.isComplete && (Time.time > (trick.endTime + trickUILinger)))
+            {
+                // trick has been dispalyed for long enough, plz remove
+                Destroy(fab);
+            }
+        }
     }
 
     private void OnDestroy()
